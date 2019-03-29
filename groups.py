b39f68f73bee86444e3a152438a6beae1f875fd0
Vladimir Komarov <vkomarov@rtlab.ru>
2019-03-29 11:14:16 +0700

    Решена проблема с отображением в кодировке cp1251 названий групп

--- groups.py
+++ groups.py
@@ -24,7 +24,9 @@ cursor = conn.cursor()
 def GroupName(group_id):
     cursor.execute("SELECT group_name FROM groups_list WHERE group_id=%s;", [group_id,])
     data = cursor.fetchone()
-    return data[0]
+    name = data[0].decode('utf-8').encode('cp1251') if wx.Platform == "__WXMSW__" else data[0]
+
+    return name
 
 
 
@@ -181,7 +183,8 @@ class GroupNameList(wx.ListCtrl):
         index = 0
         for row in cursor.fetchall():
             pos = self.InsertStringItem(index, row[0])
-            self.SetStringItem(pos, 0, row[1])
+            group_name = row[1].decode('utf-8').encode('cp1251') if wx.Platform == "__WXMSW__" else row[1]
+            self.SetStringItem(pos, 0, group_name)
             self.myRowDict[index] = row[0]
             index += 1
 
